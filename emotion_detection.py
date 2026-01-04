a210bf18cfe728c72be0091ff66897538deeca61
shivaganta-cpu <shiva.ds.bda@gmail.com>
2026-01-04 13:50:09 -0500

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,8 @@
+import requests  # Import the requests library to handle HTTP requests
+
+def emotion_detector(text_to_analyze):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyze)
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'  # URL of the sentiment analysis service
+    myobj = { "raw_document": { "text": text_to_analyze } }  # Create a dictionary with the text to be analyzed
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}  # Set the headers required for the API request
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    return response.text  # Return the response text from the API
